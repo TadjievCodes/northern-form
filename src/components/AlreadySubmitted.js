307fa171377fc990e43e039c2d9527e718e5ed65
TadjievCodes <muhammad2008.08@mail.ru>
2021-06-05 00:40:32 -0400

    Changed the text

--- src/components/AlreadySubmitted.js
+++ src/components/AlreadySubmitted.js
@@ -5,7 +5,7 @@ export default function AlreadySubmitted() {
   return (
      <section id="internship-signup" className="fr-internship-signup"> 
           <div className="fr-internship-signup-container">
-            <h1 className="fr-heading-flair">The form is already Submitted!</h1>
+            <h1 className="fr-heading-flair">Form already submitted!</h1>
              <h2 className='form-success'>Thanks for your interest!</h2>
                <p>We will review your application and contact you for additional information should your background and experience meet the requirements of one of our openings.</p>  
     </div>
